b65933d39575744e422ca1fab4ba7fa0ff6cda83
Denis <hallo-boy@mail.ru>
2023-01-05 09:03:21 +0300

    input value

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 let input = document.querySelectorAll(".input");
 let button = document.querySelector(".summ");
 let coin = [0.1, 0.2, 0.5, 1, 2, 5, 10, 20];
+
+console.log(input.length);
+
+let summarise = () => {
+  for (let i = 0; i <= input.length - 1; i++) {
+    let sum = (input[i].value * coin[i]).toFixed(2);
+    console.log(sum);
+    
+  }
+};
+
+
